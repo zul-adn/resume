f08c88cddd683c29f31f93605bcc22e3f0f3a983
zul-adn <zbojetx@gmail.com>
2024-12-31 18:47:15 +0700

    fix: add link

--- modules/home/components/portofolio/index.tsx
+++ modules/home/components/portofolio/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import useHome from '../../hooks/useHome'
+import Link from 'next/link';
 
 type Props = {}
 
@@ -11,8 +12,13 @@ function Portofolio({}: Props) {
     }, [])
 
   return (
-    <div>{portofolio && portofolio.map((porto: any, index: number) => <div key={index}>
-        {porto.type}
+    <div>{portofolio && portofolio.map((porto: any, index: number) => <div key={index} className='mb-2'>
+        <h3 className='font-bold text-md'>{porto.type}</h3>
+        <div>{porto.portofolio.map((val:any, i: number) => 
+          <div key={i} >
+            {val.name} - <Link href={val.url} target='__blank' className='text-sm text-blue-700 italic'> open link </Link>
+          </div>
+        )}</div>
     </div>)}</div>
   )
 }
